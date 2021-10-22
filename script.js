a7e99606f7ee1a6d3a5d5357d070171056990b60
vaibhav <sharmavaibhav9215@gmail.com>
2021-10-23 00:22:41 +0530

    hi

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 var settingsmenu = document.querySelector(".settings-menu");
-var darkBtn= document.getElementById("dark-btn");
+var darkBtn = document.getElementById("dark-btn");
 
-function settingMenuToggle(){
-    settingsmenu.classList.toggle("settings-menu-height");    
+function settingMenuToggle() {
+    settingsmenu.classList.toggle("settings-menu-height");
 }
-darkBtn.onclick = function(){
+darkBtn.onclick = function () {
     darkBtn.classList.toggle("dark-btn-on");
     document.body.classList.toggle("dark-theme");
-    
-    if(localStorage.getItem("theme") == "light"){
-        localStorage.setItem("theme" , "dark");
+
+    if (localStorage.getItem("theme") == "light") {
+        localStorage.setItem("theme", "dark");
     }
-    else{
-        localStorage.setItem("theme" , "light");
+    else {
+        localStorage.setItem("theme", "light");
     }
 }
 
-if(localStorage.getItem("theme") == "light"){
+if (localStorage.getItem("theme") == "light") {
     darkBtn.classList.remove("dark-btn-on");
     document.body.classList.remove("dark-theme");
 }
-else if(localStorage.getItem("theme") == "dark"){
+else if (localStorage.getItem("theme") == "dark") {
     darkBtn.classList.add("dark-btn-on");
     document.body.classList.add("dark-theme");
 }
-else{
-    localStorage.setItem("theme" , "light");
+else {
+    localStorage.setItem("theme", "light");
 }
 
 
